27e1e9996018ef86a2ed19e7264bce7506485bbf
samwil <samuelgost4@gmail.com>
2023-09-17 09:43:33 -0700

    Critical Thinking Assignment Part 2

---
+++ module1Part2/main.py
@@ -0,0 +1,4 @@
+num1 = int(input('Please enter the first number '))
+num2 = int(input('Please enter the second number '))
+print('The multiplication of these two number is', num1*num2)
+print('The division of these two number is', num1/num2)
