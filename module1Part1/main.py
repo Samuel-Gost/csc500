319df2c369e2b56d1736ac1770bebc59fdb77d7b
samwil <samuelgost4@gmail.com>
2023-09-17 09:41:03 -0700

    critical thinking assignment par 1

---
+++ module1Part1/main.py
@@ -0,0 +1,5 @@
+num1 = int(input('Please enter the first number '))
+num2 = int(input('Please enter the second number '))
+print('The addition of these two number is', num1+num2)
+print('The subtraction of these two number is', num1-num2)
+
