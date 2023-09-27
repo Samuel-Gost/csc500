5bca9f80caf420567cabf35c7dd95a31d6338075
samwil <samuelgost4@gmail.com>
2023-09-27 12:58:07 -0700

    adding space

--- module-3-part-2/main.py
+++ module-3-part-2/main.py
@@ -2,3 +2,4 @@ hourNow = int(input('What is the hour currently? '))
 hoursAdded = int(input('How many hours after this hour you want to wait? '))
 finalHour = (hoursAdded + hourNow) % 24
 print('The time the clock will go off is', finalHour)
+
