5251118d586166fdf74a9d3a6f0591aebff71e41
Samuel-Gost <145281237+Samuel-Gost@users.noreply.github.com>
2023-10-25 21:36:17 -0700

    Delete module1Part1 directory

--- module1Part1/main.py
+++
@@ -1,5 +0,0 @@
-num1 = int(input('Please enter the first number '))
-num2 = int(input('Please enter the second number '))
-print('The addition of these two number is', num1+num2)
-print('The subtraction of these two number is', num1-num2)
-
